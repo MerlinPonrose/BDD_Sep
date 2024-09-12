4dabb47c1afc6ea556d2ed81f7884f07e8b7e6d4
unknown <mrlnrose6@gmail.com>
2024-09-12 21:01:17 +0530

    plugin

--- src/test/java/com/bdd/runnerfile/TestRunner.java
+++ src/test/java/com/bdd/runnerfile/TestRunner.java
@@ -7,7 +7,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\neworkspace\\BDDFramework_Aug\\src\\test\\java\\com\\bdd\\feature",
-glue="com.bdd.stepdefinition")
+glue="com.bdd.stepdefinition",
+plugin= {"pretty","html:target/cucumber","json:target/cucumber.json"})
 public class TestRunner {
 
 }
